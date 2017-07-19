5a2cb703913c205321d01cda85cf079c0ae0a830
Aidar Babanov <aidar.babanov@nu.edu.kz>
2017-07-19 17:22:35 +0600

    Deleted app bar

--- app/src/main/java/com/example/aidar/knowledgedb/Activities/MainActivity.java
+++ app/src/main/java/com/example/aidar/knowledgedb/Activities/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -18,13 +20,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        //Remove title bar
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//
+//        //Remove notification bar
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
         setContentView(R.layout.activity_main);
+
         searchCompanyEditText = (EditText) findViewById(R.id.search_company_editText);
         searchCompanyButton = (Button) findViewById(R.id.search_company_button);
     }
 
     public void SearchCompany(View view) {
-        Intent intentToStartCompanyActivity  = new Intent(this, CompanyActivity.class);
+        Intent intentToStartCompanyActivity = new Intent(this, CompanyActivity.class);
         intentToStartCompanyActivity.putExtra("", "");
         startActivity(intentToStartCompanyActivity);
     }
