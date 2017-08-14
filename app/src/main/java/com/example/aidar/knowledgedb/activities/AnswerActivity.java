93a26bd82ab50b8cdbf2baa123e5f6f3c1cdce2e
Aidar Babanov <aidar.babanov@nu.edu.kz>
2017-08-15 02:58:23 +0600

    Hiding action bar I did it somehow

--- app/src/main/java/com/example/aidar/knowledgedb/activities/AnswerActivity.java
+++ app/src/main/java/com/example/aidar/knowledgedb/activities/AnswerActivity.java
@@ -1,11 +1,15 @@
 package com.example.aidar.knowledgedb.activities;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
@@ -18,7 +22,7 @@ public class AnswerActivity extends AppCompatActivity {
     Toolbar mToolbar;
     TextView answerTextView;
     NestedScrollView scrollView;
-
+    int actionBarHeight;
     private static final int HIDE_THRESHOLD = 20;
     private int scrolledDistance = 0;
     private boolean controlsHidden = false;
@@ -44,32 +48,52 @@ public class AnswerActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         //setTitle(getString(R.string.app_name));
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
         mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true)) {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+        }
 
         answerTextView = (TextView) findViewById(R.id.answerTextView);
         scrollView = (NestedScrollView) findViewById(R.id.scrollView);
         scrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
             @Override
             public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                int dx=oldScrollX-scrollX;
-                int dy=oldScrollY-scrollY;
-                
+                int dx = oldScrollX - scrollX;
+                int dy = oldScrollY - scrollY;
+
+                int top = v.getScrollY() - actionBarHeight;
+
                 if (scrolledDistance > HIDE_THRESHOLD && controlsHidden) {
                     onShow();
                     controlsHidden = false;
                     scrolledDistance = 0;
-                } else if (scrolledDistance < -HIDE_THRESHOLD && !controlsHidden) {
+                } else if (top >= 0 && scrolledDistance < -HIDE_THRESHOLD && !controlsHidden) {
                     onHide();
                     controlsHidden = true;
                     scrolledDistance = 0;
                 }
 
-                if((controlsHidden && dy>0) || (!controlsHidden && dy<0)) {
+                if ((controlsHidden && dy > 0) || (!controlsHidden && dy < 0)) {
                     scrolledDistance += dy;
                 }
             }
-            public void onHide(){hideViews();}
-            public void onShow(){showViews();}
+
+
+            public void onHide() {
+                hideViews();
+            }
+
+            public void onShow() {
+                showViews();
+            }
+
         });
 //        helpedButton = (Button) findViewById(R.id.helped_button);
 //        notHelpedButton = (Button) findViewById(R.id.not_helped_button);
@@ -165,7 +189,7 @@ public class AnswerActivity extends AppCompatActivity {
                 onBackPressed();
                 return true;
         }
-        return(super.onOptionsItemSelected(item));
+        return (super.onOptionsItemSelected(item));
     }
 
     private void hideViews() {
