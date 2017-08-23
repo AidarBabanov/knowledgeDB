59531eed0c06f6a4d75df429e8f11c7c5afcf73e
Aidar Babanov <aidar.babanov@nu.edu.kz>
2017-08-23 16:38:05 +0600

    v 1.1

--- app/src/main/java/com/nFactorial/aidar/knowledgedb/fragments/MenuFragment.java
+++ app/src/main/java/com/nFactorial/aidar/knowledgedb/fragments/MenuFragment.java
@@ -102,16 +102,16 @@ public class MenuFragment extends Fragment implements RecyclerViewAdapter.Recycl
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException(context.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
 
     @Override
     public void onDetach() {
